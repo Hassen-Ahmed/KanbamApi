717326649decbe245548cdc06241dc1f4d468245
Hassen-Ahmed <hassenbest23@gmail.com>
2024-04-12 22:29:52 +0100

    Create UserRegistration Model which represent users registation details.

---
+++ Models/AuthModels/UserRegistration.cs
@@ -0,0 +1,11 @@
+namespace KanbamApi.Models.AuthModels;
+public class UserRegistration
+{
+    public string? Email { get; set; } = "";
+    public string? Password { get; set; } = "";
+    public string? PasswordConfirm { get; set; } = "";
+
+}
+
+
+
