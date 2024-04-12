336dce07b43169cfe9de9b654a17f68493eda48d
Hassen-Ahmed <hassenbest23@gmail.com>
2024-04-12 22:32:06 +0100

    Create Card Model that represent Cards Collection documents of mongodb's Cards Collection document.

---
+++ Models/Card.cs
@@ -0,0 +1,14 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace KanbamApi.Models;
+
+public class Card {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+    public string? ListId { get; set;} = null!;
+    public int IndexNumber { get; set;}
+    public string? Title { get; set;} = "";
+    public Boolean IsDragging { get; set; }
+}
