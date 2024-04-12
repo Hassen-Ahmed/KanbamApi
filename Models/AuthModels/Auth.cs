7285af0c81513b63069c6c8fc6dad822ceb99c3a
Hassen-Ahmed <hassenbest23@gmail.com>
2024-04-12 22:27:42 +0100

    Create new Auth Models that represent Auth Collection of mongodb Auth Collection.

---
+++ Models/AuthModels/Auth.cs
@@ -0,0 +1,20 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace KanbamApi.Models;
+public class Auth { 
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+    public string? Email { get; set;} = null!;
+    [BsonRepresentation(BsonType.Binary)]
+    public byte[] PasswordHash { get; set; }
+    [BsonRepresentation(BsonType.Binary)]
+    public byte[] PasswordSalt { get; set; }
+
+     public Auth()
+    {
+        PasswordHash ??= [];
+        PasswordSalt ??= [];
+    }
+}
