5e940e80ef67ea1103746a60778cc77c408e725c
Hassen-Ahmed <hassenbest23@gmail.com>
2024-04-13 12:56:41 +0100

    Add and modify Program.cs file with loading DotEnv, AddSingleton service to DI, AddControllers, AddCors, AddAuthentication, UseCors.

--- Program.cs
+++ Program.cs
@@ -1,18 +1,81 @@
+using System.Text;
+using KanbamApi.Repo;
+using KanbamApi.Services;
+using KanbamApi.Services.AuthServices;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+// load .env file variables
+DotNetEnv.Env.Load();
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<AuthService>();
+builder.Services.AddScoped<IAuthControllerService, AuthControllerService>();
+
+builder.Services.AddSingleton<UsersService>();
+builder.Services.AddSingleton<ListsService>();
+builder.Services.AddSingleton<CardsService>();
+builder.Services.AddSingleton<KanbamDbRepository>();
+
+builder.Services.AddControllers();
+
+builder.Services.AddCors((options) =>
+{
+    options.AddPolicy("DevCors", (corsBuilder) =>
+    { 
+        corsBuilder.WithOrigins("http://localhost:4200", "http://localhost:3000", "http://localhost:8000")
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials()
+        ;
+    });
+
+    options.AddPolicy("ProdCors", (corsBuilder) =>
+    { 
+        corsBuilder.WithOrigins("https://kanbamapi.onrender.com")
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials()
+        ;
+    });
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(
+    options =>
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true, // true ==>  later after hosted
+            ValidateAudience = true, // true ==>  later after hosted
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(
+                    DotNetEnv.Env.GetString("TOKEN_KEY")
+                )
+            ),
+            ValidIssuer = DotNetEnv.Env.GetString("VALID_ISSUER"), 
+            ValidAudience = DotNetEnv.Env.GetString("VALID_AUDIENCE"),
+            
+        };
+    }
+);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseCors("DevCors");
     app.UseSwagger();
     app.UseSwaggerUI();
 } else {
+    app.UseCors("ProdCors");
     app.UseHttpsRedirection();
 }
 
