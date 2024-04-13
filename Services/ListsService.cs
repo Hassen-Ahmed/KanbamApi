a5d298c1cd871e60f6754a22a7b0351e53a37c96
Hassen-Ahmed <hassenbest23@gmail.com>
2024-04-13 22:12:42 +0100

    Modify ListsService with new mothods to manuplate Lists collection to the KanbamDb.

--- Services/ListsService.cs
+++ Services/ListsService.cs
@@ -6,13 +6,18 @@ namespace KanbamApi.Services;
 
 public class ListsService
 {
-    private readonly IMongoCollection<List> _listsCollection;
-    public ListsService( KanbamDbRepository kanbamDbRepository)
-    {
+        private readonly IMongoCollection<List> _listsCollection;
+        public ListsService( KanbamDbRepository kanbamDbRepository)
+        {
             _listsCollection = kanbamDbRepository.kanbamDatabase.GetCollection<List>(DotNetEnv.Env.GetString("LISTS_COLLECTION_NAME"));
-    }
+        }
 
-   public async Task<List<List>> GetAsync() =>
+        public async Task<List<List>> GetAsync() =>
         await _listsCollection.Find(_ => true).ToListAsync();
 
+        public async Task CreateAsync(List newList) {
+        await _listsCollection.InsertOneAsync(newList);
+                }
+        public async Task RemoveAsync(string id) =>
+        await _listsCollection.DeleteOneAsync(x => x.Id == id);
 }
