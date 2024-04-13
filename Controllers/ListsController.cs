9e4a3074464be130d17b7863e07e3abdf845cbff
Hassen-Ahmed <hassenbest23@gmail.com>
2024-04-13 22:13:27 +0100

    Modify ListsController with POST and DELETE http request handler.

--- Controllers/ListsController.cs
+++ Controllers/ListsController.cs
@@ -7,11 +7,29 @@ namespace KanbamApi.Controllers;
 [ApiController]
 [Route("api/[controller]")]
 public class ListsController : ControllerBase {
-private readonly ListsService _listsService;
-    public ListsController(ListsService listsService) =>
+    private readonly ListsService _listsService;
+    private readonly CardsService _cardsService;
+    public ListsController(ListsService listsService,CardsService cardsService) {
         _listsService = listsService;
+        _cardsService = cardsService;
+    }
 
    [HttpGet]
     public async Task<List<List>> Get() =>
         await _listsService.GetAsync();
+
+    [HttpPost]
+    public async Task<IActionResult> Post(List newList) {
+        await _listsService.CreateAsync(newList);
+        return CreatedAtAction(nameof(Get), new { id = newList.Id }, newList);
+    } 
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete(string listId) {
+
+        await _listsService.RemoveAsync(listId);
+        await _cardsService.RemoveManyByListIdAsync(listId);
+        
+        return NoContent();
+    }
 }
