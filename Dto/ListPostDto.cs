172b5d57f57abac51d8bca65e7dbe092890d71be
Hassen-Ahmed <hassenbest23@gmail.com>
2024-04-16 15:52:26 +0100

    Create ListsPostDto and we going to use it wiht List modle by mapping.

---
+++ Dto/ListPostDto.cs
@@ -0,0 +1,12 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace KanbamApi.Models;
+
+public class ListPostDto {
+    public string? Title { get; set;} 
+    public int IndexNumber { get; set;}
+    public Boolean? IsDragging { get; set; } = false;
+}
+
+
