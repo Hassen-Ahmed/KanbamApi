9e009c54e89eae5eefef9cd5b335ca296b0a6e69
Hassen-Ahmed <hassenbest23@gmail.com>
2024-05-02 14:35:50 +0100

    Authorize this controller create new GetListsWithCardsByUserId method for handle to get all Lists with Cards as aggregate form using UserId that stored in Authentication Jwt and also modify other http request.

--- Controllers/ListsController.cs
+++ Controllers/ListsController.cs
@@ -1,45 +1,87 @@
 
 using KanbamApi.Models;
 using KanbamApi.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 
 namespace KanbamApi.Controllers;
+[Authorize]
 [ApiController]
 [Route("api/[controller]")]
 public class ListsController : ControllerBase {
     private readonly ListsService _listsService;
     private readonly CardsService _cardsService;
-    public ListsController(ListsService listsService,CardsService cardsService) {
+    private readonly UsersService _usersService;
+    public ListsController(ListsService listsService,CardsService cardsService,UsersService usersService ) {
         _listsService = listsService;
         _cardsService = cardsService;
+        _usersService = usersService;
+    }
+   
 
-        //   _mapper = new Mapper(new MapperConfiguration(
-        //     cfg => cfg.CreateMap<UserRegistration, UserComplete>()
-        // ));
+
+    [Authorize]
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<object>>> GetListsWithCardsByUserId() {
+            
+            try
+                {
+                var userId = User.FindFirst("userId")?.Value;
+                var res = await _listsService.GetListsWithCardsByUserId(userId!);
+                return Ok(res);
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
     }
 
-    [HttpGet]
-    public async Task<List<List>> Get() =>
-        await _listsService.GetAsync();
-
+    [Authorize]
     [HttpPost]
     public async Task<IActionResult> CreateList(List newList) {
-        await _listsService.CreateAsync(newList);
-        return CreatedAtAction(nameof(CreateList), new { id = newList.Id }, newList);
+        try
+        {    
+            var userId = User.FindFirst("userId")?.Value;
+            newList.UserId = userId;
+            await _listsService.CreateAsync(newList);
+            return CreatedAtAction(nameof(CreateList), new { id = newList.Id }, newList);
+          }
+        catch (System.Exception)
+        {
+            throw;
+        }
     } 
     
+    [Authorize]
     [HttpPut("{id}")]
     public async Task<IActionResult> UpdateList(string id ,List updatedList) {
+        try
+        {
             await _listsService.UpdateAsync(id, updatedList);
             return CreatedAtAction(nameof(UpdateList), new { id = updatedList.Id }, updatedList);
+        }
+        catch (System.Exception)
+        {
+            
+            throw;
+        }
     }
 
+
+    [Authorize]
     [HttpDelete("{listId}")]
     public async Task<IActionResult> RemoveList(string listId) {
-
-        await _listsService.RemoveAsync(listId);
-        await _cardsService.RemoveManyByListIdAsync(listId);
-        
-        return NoContent();
+        try
+        {
+            await _listsService.RemoveAsync(listId);
+            await _cardsService.RemoveManyByListIdAsync(listId);
+            
+            return NoContent(); 
+        }
+        catch (System.Exception)
+        {
+            throw;
+        }
     }
 }
