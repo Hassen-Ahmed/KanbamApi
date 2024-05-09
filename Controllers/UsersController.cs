70c15be90a20c7c3dd292e825da0fb6dc29cd636
Hassen-Ahmed <hassenbest23@gmail.com>
2024-05-09 20:52:11 +0100

    Comment out all codes that exist in this UsersControllers.

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,22 +1,22 @@
 
 
-using KanbamApi.Models;
-using KanbamApi.Services;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+// using KanbamApi.Models;
+// using KanbamApi.Services;
+// using Microsoft.AspNetCore.Authorization;
+// using Microsoft.AspNetCore.Mvc;
 
-namespace KanbamApi.Controllers;
+// namespace KanbamApi.Controllers;
 
-[Authorize]
-[ApiController]
-[Route("api/[controller]")]
-public class UsersController : ControllerBase {
-private readonly UsersService _usersService;
-    public UsersController(UsersService usersService) =>
-        _usersService = usersService;
+// [Authorize]
+// [ApiController]
+// [Route("api/[controller]")]
+// public class UsersController : ControllerBase {
+// private readonly UsersService _usersService;
+//     public UsersController(UsersService usersService) =>
+//         _usersService = usersService;
 
-    [Authorize]
-    [HttpGet]
-    public async Task<List<User>> Get() =>
-        await _usersService.GetAllUserAsync();
-}
+//     // [Authorize]
+//     // [HttpGet]
+//     // public async Task<List<User>> Get() =>
+//     //     await _usersService.GetAllUserAsync();
+// }
