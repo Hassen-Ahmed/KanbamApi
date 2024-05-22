de2e42323096d004e8ca2441ccd5f7715f0eb2a7
Hassen-Ahmed <hassenbest23@gmail.com>
2024-05-22 13:02:26 +0100

    Create new  TestBase with its IAsyncLifetime abstract/ interface.

---
+++ Kanbam.Test/Reset/TestBase.cs
@@ -0,0 +1,32 @@
+using KanbamApi.Data;
+using KanbamApi.Data.Seed;
+
+namespace Kanbam.Test.Reset;
+
+public class TestBase : IAsyncLifetime
+{
+    public TestBase()
+    {
+        string projectDir = Directory
+            .GetParent(Directory.GetCurrentDirectory())
+            ?.Parent?.Parent?.Parent?.FullName!;
+
+        string envFilePath = Path.Combine(projectDir, "KanbamApi", ".env.test");
+
+        if (File.Exists(envFilePath))
+            DotNetEnv.Env.Load(envFilePath);
+    }
+
+    public async Task InitializeAsync()
+    {
+        var kanbamDbContext = new KanbamDbContext();
+        var mongoDbSeeder = new MongoDbSeeder(kanbamDbContext);
+        await mongoDbSeeder.SeedAsync();
+    }
+
+    public Task DisposeAsync()
+    {
+        // Implement if you need any cleanup after all tests in a derived class run
+        return Task.CompletedTask;
+    }
+}
