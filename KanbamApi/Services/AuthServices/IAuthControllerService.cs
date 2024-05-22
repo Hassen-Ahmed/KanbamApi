442940fd487c3898528e6c3f157ea8f1fe9dabca
Hassen-Ahmed <hassenbest23@gmail.com>
2024-05-22 12:58:48 +0100

    Modify This Services and Validator namespaces and directors.

--- KanbamApi/Services/AuthServices/IAuthControllerService.cs
+++
@@ -1,7 +0,0 @@
-namespace KanbamApi.Services.AuthServices;
-
-public interface IAuthControllerService
-{
-    byte[] GeneratePasswordHash(string password, byte[] passwordSalt);
-    string GenerateToken(string userId);
-}
