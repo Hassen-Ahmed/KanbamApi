29c531d0c682b43b7488545d6405c271ef0697aa
Hassen-Ahmed <hassenbest23@gmail.com>
2024-05-27 12:57:40 +0100

    Add tests for TestLoginAuthController for 400, 401 x3 .

--- Kanbam.Test/Controllers/TestLoginAuthController.cs
+++ Kanbam.Test/Controllers/TestLoginAuthController.cs
@@ -38,7 +38,7 @@ public class TestLoginAuthController : TestBase
     }
 
     [Fact]
-    public async Task Login_OnValidUserDetail_ReturnOkResult_200()
+    public async Task Login_OnValidUserDetail_ReturnOkResult_200_WithTokenkey()
     {
         // Assign
         UserLogin validUser = UserLoginFixture.ValidUser();
@@ -82,5 +82,169 @@ public class TestLoginAuthController : TestBase
         result.StatusCode.Should().Be(201);
         token.Should().Be("this is my token.");
         _validatLoginMock.Verify(a => a.ValidateAsync(validUser, default), Times.Once);
+    }
+
+    [Fact]
+    public async Task Login_OnInValidUserDetail_Return_400_BadRequest()
+    {
+        // Assign
+        UserLogin validUser = UserLoginFixture.InValidUser();
+
+        var validationResult = new ValidationResult();
+        validationResult.Errors.Add(new ValidationFailure("Email", "Invalid credential!"));
+
+        _validatLoginMock
+            .Setup(x => x.ValidateAsync(validUser, default))
+            .ReturnsAsync(validationResult);
+
+        _authRepoMock
+            .Setup(a => a.CheckEmailExist(It.IsAny<string>()))
+            .ReturnsAsync(
+                new Auth()
+                {
+                    Email = "test@gmail.com",
+                    PasswordHash = new byte[128 / 8],
+                    PasswordSalt = new byte[128 / 8]
+                }
+            );
+
+        _authRepoMock.Setup(a => a.CreateAsync(It.IsAny<Auth>())).ReturnsAsync(true);
+
+        _userRepoMock
+            .Setup(u => u.GetUserIdAsync(It.IsAny<string>()))
+            .ReturnsAsync("userIdLength>0");
+
+        _authControllerServiceMock
+            .Setup(a => a.GeneratePasswordHash(It.IsAny<string>(), It.IsAny<byte[]>()))
+            .Returns(new byte[128 / 8]);
+
+        _authControllerServiceMock
+            .Setup(a => a.GenerateToken(It.IsAny<string>()))
+            .Returns("this is my token.");
+
+        // Act
+        var result = (ObjectResult)await _authController.Login(validUser);
+
+        // Assert
+        result.StatusCode.Should().Be(400);
+        result.Value?.ToString().Should().BeSameAs("Invalid credential!");
+    }
+
+    [Fact]
+    public async Task Login_OnEmailNotExist_Return_401_BadRequest()
+    {
+        // Assign
+        UserLogin validUser = UserLoginFixture.ValidUser();
+
+        _validatLoginMock
+            .Setup(x => x.ValidateAsync(validUser, default))
+            .ReturnsAsync(new ValidationResult());
+
+        _authRepoMock.Setup(a => a.CheckEmailExist(It.IsAny<string>())).ReturnsAsync((Auth)null!);
+
+        _authRepoMock.Setup(a => a.CreateAsync(It.IsAny<Auth>())).ReturnsAsync(true);
+
+        _userRepoMock
+            .Setup(u => u.GetUserIdAsync(It.IsAny<string>()))
+            .ReturnsAsync("userIdLength>0");
+
+        _authControllerServiceMock
+            .Setup(a => a.GeneratePasswordHash(It.IsAny<string>(), It.IsAny<byte[]>()))
+            .Returns(new byte[128 / 8]);
+
+        _authControllerServiceMock
+            .Setup(a => a.GenerateToken(It.IsAny<string>()))
+            .Returns("this is my token.");
+
+        // Act
+        var result = (ObjectResult)await _authController.Login(validUser);
+
+        // Assert
+        result.StatusCode.Should().Be(401);
+        result.Value?.ToString().Should().Be("Wrong Email address.");
+    }
+
+    [Fact]
+    public async Task Login_OnWrongPasswordHash_Return_401_UnAuthorized()
+    {
+        // Assign
+        UserLogin validUser = UserLoginFixture.ValidUser();
+
+        _validatLoginMock
+            .Setup(x => x.ValidateAsync(validUser, default))
+            .ReturnsAsync(new ValidationResult());
+
+        _authRepoMock
+            .Setup(a => a.CheckEmailExist(It.IsAny<string>()))
+            .ReturnsAsync(
+                new Auth()
+                {
+                    Email = "test@gmail.com",
+                    PasswordHash = new byte[128 / 8],
+                    PasswordSalt = new byte[128 / 8]
+                }
+            );
+
+        _authRepoMock.Setup(a => a.CreateAsync(It.IsAny<Auth>())).ReturnsAsync(true);
+
+        _userRepoMock
+            .Setup(u => u.GetUserIdAsync(It.IsAny<string>()))
+            .ReturnsAsync("userIdLength>0");
+
+        _authControllerServiceMock
+            .Setup(a => a.GeneratePasswordHash(It.IsAny<string>(), It.IsAny<byte[]>()))
+            .Returns([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+
+        _authControllerServiceMock
+            .Setup(a => a.GenerateToken(It.IsAny<string>()))
+            .Returns("this is my token.");
+
+        // Act
+        var result = (ObjectResult)await _authController.Login(validUser);
+
+        // Assert
+        result.StatusCode.Should().Be(401);
+        result.Value?.ToString().Should().Be("Unauthorized Request!");
+    }
+
+    [Fact]
+    public async Task Login_OnWrongUserId_Return_401_UnAuthorized()
+    {
+        // Assign
+        UserLogin validUser = UserLoginFixture.ValidUser();
+
+        _validatLoginMock
+            .Setup(x => x.ValidateAsync(validUser, default))
+            .ReturnsAsync(new ValidationResult());
+
+        _authRepoMock
+            .Setup(a => a.CheckEmailExist(It.IsAny<string>()))
+            .ReturnsAsync(
+                new Auth()
+                {
+                    Email = "test@gmail.com",
+                    PasswordHash = new byte[128 / 8],
+                    PasswordSalt = new byte[128 / 8]
+                }
+            );
+
+        _authRepoMock.Setup(a => a.CreateAsync(It.IsAny<Auth>())).ReturnsAsync(true);
+
+        _userRepoMock.Setup(u => u.GetUserIdAsync(It.IsAny<string>())).ReturnsAsync("");
+
+        _authControllerServiceMock
+            .Setup(a => a.GeneratePasswordHash(It.IsAny<string>(), It.IsAny<byte[]>()))
+            .Returns(new byte[128 / 8]);
+
+        _authControllerServiceMock
+            .Setup(a => a.GenerateToken(It.IsAny<string>()))
+            .Returns("this is my token.");
+
+        // Act
+        var result = (ObjectResult)await _authController.Login(validUser);
+
+        // Assert
+        result.StatusCode.Should().Be(401);
+        result.Value?.ToString().Should().Be("Unauthorized User!");
     }
 }
