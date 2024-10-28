790c1afe74527e926b3a8175190e4b164986bfac
Hassen-Ahmed <hassenbest23@gmail.com>
2024-10-28 13:41:17 +0000

    feat(back-end): modified these business layer by defining new methods to handle data modification and pass them to controllers, and also modified theirs interfaces(contracts)

--- KanbamApi/Services/Interfaces/IUsersService.cs
+++ KanbamApi/Services/Interfaces/IUsersService.cs
@@ -1,3 +1,13 @@
+using KanbamApi.Dtos.Update;
+using KanbamApi.Models;
+
 namespace KanbamApi.Services.Interfaces;
 
-public interface IUsersService { }
+public interface IUsersService
+{
+    Task<List<User>> GetAllAsync();
+    Task<List<User>> GetByIdAsync(string id);
+    Task<string> GetUserIdByEmail(string id);
+    Task<string?> CreateAsync(User newUser);
+    Task<bool> PatchByIdAsync(string userId, DtoUsersUpdate updateUser);
+}
