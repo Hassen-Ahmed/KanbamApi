55c7acaf33ff7a183c4b1342f89cd1e032284951
Hassen-Ahmed <hassenbest23@gmail.com>
2024-10-28 17:15:02 +0000

    feat(back-end): modified CardsController by swapping cardsRepo with cardsService for separetion concern, and create new methods to Patch and Delete based on parametrs

--- KanbamApi/Controllers/CardsControllers.cs
+++ KanbamApi/Controllers/CardsControllers.cs
@@ -1,8 +1,11 @@
 using KanbamApi.Dtos;
+using KanbamApi.Dtos.Update;
 using KanbamApi.Models;
 using KanbamApi.Repositories.Interfaces;
+using KanbamApi.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 
 namespace KanbamApi.Controllers;
 
@@ -11,104 +14,109 @@ namespace KanbamApi.Controllers;
 [Route("api/[controller]")]
 public class CardsController : ControllerBase
 {
-    private readonly ICardsRepo _cardsRepo;
+    private readonly ICardsService _cardsService;
 
-    public CardsController(ICardsRepo cardsRepo) => _cardsRepo = cardsRepo;
+    public CardsController(ICardsService cardsService) => _cardsService = cardsService;
 
-    [Authorize]
     [HttpGet("{listId}/list")]
     public async Task<ActionResult<List<Card>>> GetByListId(string listId)
     {
+        if (!ObjectId.TryParse(listId, out var _))
+        {
+            return BadRequest("Invalid listId.");
+        }
         try
         {
-            return await _cardsRepo.GetByListIdAsync(listId);
+            var cards = await _cardsService.GetByListIdAsync(listId);
+            return Ok(new Dictionary<string, object> { { "cards", cards } });
         }
-        catch (Exception)
+        catch (Exception ex)
         {
-            return StatusCode(
-                StatusCodes.Status500InternalServerError,
-                "An error occurred while processing the request."
-            );
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
         }
     }
 
-    [Authorize]
     [HttpGet("{cardId}/card")]
     public async Task<ActionResult<List<Card>>> GetByCardId(string cardId)
     {
+        if (!ObjectId.TryParse(cardId, out var _))
+        {
+            return BadRequest("Invalid cardId.");
+        }
         try
         {
-            return await _cardsRepo.GetByCardIdAsync(cardId);
+            var card = await _cardsService.GetByIdAsync(cardId);
+            return Ok(new Dictionary<string, object> { { "card", card } });
         }
-        catch (Exception)
+        catch (Exception ex)
         {
-            return StatusCode(
-                StatusCodes.Status500InternalServerError,
-                "An error occurred while processing the request."
-            );
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
         }
     }
 
-    [Authorize]
     [HttpPost]
-    public async Task<IActionResult> CreateNewCard(DtoCardPost newCard)
+    public async Task<IActionResult> CreateNewCard(DtoCardPost dtoNewCard)
     {
         try
         {
-            Card card =
-                new()
-                {
-                    ListId = newCard.ListId,
-                    Title = newCard.Title,
-                    IndexNumber = newCard.IndexNumber,
-                    StartDate = newCard.StartDate,
-                };
-
-            var createdCard = await _cardsRepo.CreateAsync(card);
+            var createdCard = await _cardsService.CreateAsync(dtoNewCard);
             return StatusCode(201, createdCard);
         }
-        catch (Exception)
+        catch (Exception ex)
         {
-            return StatusCode(
-                StatusCodes.Status500InternalServerError,
-                "An error occurred while processing the request."
-            );
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
         }
     }
 
-    [Authorize]
-    [HttpPut("{id}")]
-    public async Task<IActionResult> UpdateCard(string id, Card updatedCard)
+    [HttpPatch("{id}")]
+    public async Task<IActionResult> UpdateCard(string id, DtoCardUpdate dtoCardUpdate)
     {
+        if (!ObjectId.TryParse(id, out var _))
+        {
+            return BadRequest("Invalid id.");
+        }
+
+        var updated = await _cardsService.PatchByIdAsync(id, dtoCardUpdate);
+
+        if (!updated)
+            return NotFound("Card not found or nothing to update.");
+
+        return NoContent();
+    }
+
+    [HttpDelete("{cardId}/card")]
+    public async Task<IActionResult> RemoveById(string cardId)
+    {
+        if (!ObjectId.TryParse(cardId, out var _))
+        {
+            return BadRequest("Invalid cardId.");
+        }
         try
         {
-            await _cardsRepo.UpdateAsync(id, updatedCard);
-            return CreatedAtAction(nameof(UpdateCard), new { id = updatedCard.Id }, updatedCard);
+            var res = await _cardsService.RemoveByIdAsync(cardId);
+            return res ? NoContent() : BadRequest();
         }
-        catch (Exception)
+        catch (Exception ex)
         {
-            return StatusCode(
-                StatusCodes.Status500InternalServerError,
-                "An error occurred while processing the request."
-            );
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
         }
     }
 
-    [Authorize]
-    [HttpDelete("{cardId}")]
-    public async Task<IActionResult> RemoveList(string cardId)
+    [HttpDelete("{listId}/list")]
+    public async Task<IActionResult> RemoveByListId(string listId)
     {
+        if (!ObjectId.TryParse(listId, out var _))
+        {
+            return BadRequest("Invalid listId.");
+        }
         try
         {
-            await _cardsRepo.RemoveAsync(cardId);
-            return NoContent();
+            var res = await _cardsService.RemoveByListIdAsync(listId);
+            return res ? NoContent() : BadRequest();
         }
-        catch (Exception)
+        catch (Exception ex)
         {
-            return StatusCode(
-                StatusCodes.Status500InternalServerError,
-                "An error occurred while processing the request."
-            );
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
         }
     }
 }
