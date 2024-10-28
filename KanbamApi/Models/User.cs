1bd9733a2c9613df2b4f5326fb895f93115d3f24
Hassen-Ahmed <hassenbest23@gmail.com>
2024-10-28 14:00:46 +0000

    feat(back-end): removed nullability from Id member

--- KanbamApi/Models/User.cs
+++ KanbamApi/Models/User.cs
@@ -7,7 +7,7 @@ public class User
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-    public string? Id { get; set; }
+    public string Id { get; set; }
 
     [BsonRequired]
     [BsonElement("Email")]
