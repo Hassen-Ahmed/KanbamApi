3da21774608def2cf2417edd8ef4da4d6e0baa94
Hassen-Ahmed <hassenbest23@gmail.com>
2024-11-10 12:22:39 +0000

    fix(back-end): define new Email field to DtoBoardMemberPost

--- KanbamApi/Dtos/Posts/DtoBoardMemberPost.cs
+++ KanbamApi/Dtos/Posts/DtoBoardMemberPost.cs
@@ -1,12 +1,19 @@
 using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace KanbamApi.Dtos.Posts
 {
     public class DtoBoardMemberPost
     {
+        [BsonRequired]
         [Required]
         public string BoardId { get; set; } = string.Empty;
 
+        [BsonRequired]
+        [Required]
+        public string Email { get; set; } = string.Empty;
+
+        [BsonRequired]
         [Required]
         public string Role { get; set; } = string.Empty;
     }
