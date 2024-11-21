3254e0cd4a73949b0f23b2402d4e34c0a1e5bcc9
Hassen-Ahmed <hassenbest23@gmail.com>
2024-11-21 18:10:04 +0000

    fix(back-end): modified AuthRepo methods naming to make more discriptive and semantic

--- KanbamApi/Repositories/Interfaces/IAuthRepo.cs
+++ KanbamApi/Repositories/Interfaces/IAuthRepo.cs
@@ -5,6 +5,6 @@ namespace KanbamApi.Repositories.Interfaces;
 public interface IAuthRepo
 {
     Task<List<Auth>> GetAsync();
-    Task<Auth> CheckEmailExist(string? email);
+    Task<Auth> IsEmailExists(string? email);
     Task<bool> CreateAsync(Auth newAuth);
 }
