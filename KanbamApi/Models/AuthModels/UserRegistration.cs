d7e4326eb5114d149793026ec26ee68e9e43ccaa
Hassen-Ahmed <hassenbest23@gmail.com>
2024-11-21 18:25:42 +0000

    feat(back-end): added new asp.net core default validation mechanizem to UserLogin and UserRegistration members or fields

--- KanbamApi/Models/AuthModels/UserRegistration.cs
+++ KanbamApi/Models/AuthModels/UserRegistration.cs
@@ -1,8 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace KanbamApi.Models.AuthModels;
 
 public class UserRegistration
 {
-    public string? Email { get; set; }
-    public string? Password { get; set; }
+    [Required(ErrorMessage = "Email is required")]
+    [RegularExpression(
+        @"^[\w\.-]+@[a-zA-Z\d-]+(\.[a-zA-Z]{2,})+$",
+        ErrorMessage = "Wrong email address!"
+    )]
+    public string Email { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Password is required")]
+    [RegularExpression(
+        @"^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[#$@!%&*?])[A-Za-z0-9#$@!%&*?]{8,20}$",
+        ErrorMessage = "Password should be 8-20 characters and include at least 1 letter, 1 number, and only 1 special character!"
+    )]
+    public string Password { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Password confirmation is required")]
+    [Compare("Password", ErrorMessage = "Password does not match!")]
     public string? PasswordConfirm { get; set; }
 }
