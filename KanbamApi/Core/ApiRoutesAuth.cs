fad338c07c9732ea5d8d8e0deba8b5bc7618b65a
Hassen-Ahmed <hassenbest23@gmail.com>
2024-11-21 18:42:12 +0000

    feat(back-end): create new Core component ApiRoutesAuth to minimize the typo

---
+++ KanbamApi/Core/ApiRoutesAuth.cs
@@ -0,0 +1,9 @@
+namespace KanbamApi.Core;
+
+public static class ApiRoutesAuth
+{
+    public const string Register = "Registration";
+    public const string Login = "Login";
+    public const string RefreshToken = "RefreshToken";
+    public const string Revoke = "RevokeRefreshToken";
+}
