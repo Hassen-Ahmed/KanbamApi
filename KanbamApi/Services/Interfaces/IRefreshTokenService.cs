27aa348b3612180e1c983d13351f5c8afd196dc0
Hassen-Ahmed <hassenbest23@gmail.com>
2024-11-21 18:21:22 +0000

    feat(back-end): created new service (RefreshTokenService) and it interface to define business logic and also midiator between repositor and controller

---
+++ KanbamApi/Services/Interfaces/IRefreshTokenService.cs
@@ -0,0 +1,17 @@
+using KanbamApi.Models;
+using KanbamApi.Util;
+
+namespace KanbamApi.Services.Interfaces;
+
+public interface IRefreshTokenService
+{
+    Task<Result<RefreshToken>> GetRefreshTokensByTokenAsync(Guid token);
+    Task<Result<bool>> SaveRefreshTokenAsync(
+        string userId,
+        Guid refreshToken,
+        DateTime expirationDate
+    );
+
+    Task<Result<bool>> Update_RefreshToken_ById_Async(string id, Guid newToken);
+    Task<Result<bool>> DeleteRefreshTokenAsync(Guid token);
+}
