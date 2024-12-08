22aa629875d884a4143e07ac96f4a12ff928f56a
Hassen-Ahmed <hassenbest23@gmail.com>
2024-12-08 19:44:12 +0000

    feat(back-end): created new implementation to send message using IHubContext with type ListHub inside http actions

--- KanbamApi/Controllers/ListsController.cs
+++ KanbamApi/Controllers/ListsController.cs
@@ -1,8 +1,11 @@
 using KanbamApi.Dtos;
 using KanbamApi.Dtos.Update;
+using KanbamApi.Hubs;
+using KanbamApi.Models;
 using KanbamApi.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using MongoDB.Bson;
 
 namespace KanbamApi.Controllers;
@@ -14,11 +17,17 @@ public class ListsController : ControllerBase
 {
     private readonly IListsService _listsService;
     private readonly IBoardService _boardService;
+    private readonly IHubContext<ListHub> _hubContext;
 
-    public ListsController(IListsService listsService, IBoardService boardService)
+    public ListsController(
+        IListsService listsService,
+        IBoardService boardService,
+        IHubContext<ListHub> hubContext
+    )
     {
         _listsService = listsService;
         _boardService = boardService;
+        _hubContext = hubContext;
     }
 
     [HttpGet]
@@ -67,6 +76,13 @@ public class ListsController : ControllerBase
         try
         {
             var createdList = await _listsService.CreateAsync(newListDto);
+
+            var groupId = newListDto.BoardId;
+
+            await _hubContext
+                .Clients.Group(groupId)
+                .SendAsync("ReceiveListCreated", createdList, $"{groupId}");
+
             return StatusCode(201, createdList);
         }
         catch (Exception ex)
@@ -87,11 +103,26 @@ public class ListsController : ControllerBase
         if (!updated)
             return NotFound("List not found or nothing to update.");
 
+        var userId = User.FindFirst("userId")?.Value;
+        var updatedList = new List()
+        {
+            Id = listId,
+            BoardId = dtoListsUpdate.BoardId!,
+            Title = dtoListsUpdate.Title!,
+            IndexNumber = (int)dtoListsUpdate.IndexNumber!,
+        };
+
+        var groupId = dtoListsUpdate.BoardId;
+
+        await _hubContext
+            .Clients.Group(groupId!)
+            .SendAsync("ReceiveListUpdate", updatedList, userId, $"{groupId}");
+
         return NoContent();
     }
 
-    [HttpDelete("{listId}")]
-    public async Task<IActionResult> Delete(string listId)
+    [HttpDelete("{listId}/{boardId}")]
+    public async Task<IActionResult> Delete(string listId, string boardId)
     {
         if (!ObjectId.TryParse(listId, out var _))
         {
@@ -100,7 +131,17 @@ public class ListsController : ControllerBase
         try
         {
             var res = await _listsService.RemoveByIdAsync(listId);
-            return res ? NoContent() : BadRequest();
+
+            if (!res)
+                return BadRequest();
+
+            var groupId = boardId;
+
+            await _hubContext
+                .Clients.Group(groupId)
+                .SendAsync("ReceiveListDelete", listId, $"{groupId}");
+
+            return NoContent();
         }
         catch (Exception ex)
         {
