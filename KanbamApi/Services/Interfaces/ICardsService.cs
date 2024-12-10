5d8df21148c532056a63e26b57dae7f0d071a72f
Hassen-Ahmed <hassenbest23@gmail.com>
2024-12-10 21:19:11 +0000

    feat(back-end): create two card services CreateCommentAsync and RemoveCommentByIdAsync which are helps as midle layer between repo and controller

--- KanbamApi/Services/Interfaces/ICardsService.cs
+++ KanbamApi/Services/Interfaces/ICardsService.cs
@@ -9,7 +9,9 @@ public interface ICardsService
     Task<List<Card>> GetByIdAsync(string cardId);
     Task<List<Card>> GetByListIdAsync(string listId);
     Task<Card> CreateAsync(DtoCardPost dtoNewCard);
+    Task<string?> CreateCommentAsync(string cardId, Comment newComment);
     Task<bool> PatchByIdAsync(string id, DtoCardUpdate dtoCardUpdate);
     Task<bool> RemoveByIdAsync(string id);
     Task<bool> RemoveByListIdAsync(string listId);
+    Task<bool> RemoveCommentByIdAsync(string cardId, string commentId);
 }
