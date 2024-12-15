f6f1ac1fded4f62d9f4ff89c62c0a9014ecdb6f7
Hassen-Ahmed <hassenbest23@gmail.com>
2024-12-15 09:17:58 +0000

    feat(back-end): created new method GetOneByIdAsyn which receive Card data and used to pass the data to the controller. And also remove unnecessary database fetching document count

--- KanbamApi/Services/Interfaces/ICardsService.cs
+++ KanbamApi/Services/Interfaces/ICardsService.cs
@@ -8,6 +8,7 @@ public interface ICardsService
 {
     Task<List<Card>> GetByIdAsync(string cardId);
     Task<List<Card>> GetByListIdAsync(string listId);
+    Task<Card> GetOneByIdAsync(string cardId);
     Task<Card> CreateAsync(DtoCardPost dtoNewCard);
     Task<string?> CreateCommentAsync(string cardId, Comment newComment);
     Task<bool> PatchByIdAsync(string id, DtoCardUpdate dtoCardUpdate);
