879f04ecb746d5cf4d6381e93c6b764bbcb578b5
Hassen-Ahmed <hassenbest23@gmail.com>
2025-01-18 20:46:34 +0000

    feat(back-end): created ErrorMessage static class, to mitigate typos.

---
+++ KanbamApi/Core/ErrorMessages.cs
@@ -0,0 +1,11 @@
+namespace KanbamApi.Core;
+
+public static class ErrorMessages
+{
+    public const string EmailExists = "Email already exists!";
+    public const string InvalidEmail = "Invalid email address.";
+    public const string InvalidPassword = "Invalid password!";
+    public const string SuccessfullRegistered = "The registration was successfull!";
+    public const string FailedRefreshTokenUpdate = "Failed to update refresh token";
+    public const string ServerError = "Failed to update refresh token";
+}
